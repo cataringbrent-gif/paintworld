a83a03817e42600a1e8e934188c29a9c423342c9
cataringbrent-gif <cataringbrent@gmail.com>
2025-09-01 12:59:49 +0800

    gbaserhbsr

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PaintCanvas from '@/components/PaintCanvas'
-
-export default function Home() {
-  return (
-    <main className="w-full h-screen">
-      <PaintCanvas />
-    </main>
-  )
-}
